f01c692efcebe3926b3c65e66b0855b40529e40a
Zvonimir37 <zvonac50@hotmail.com>
2020-12-11 18:06:49 +0100

    Creating the Contact Models and Augment the Contact Controller

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,22 +1,26 @@
-﻿using System;
+﻿using ContactManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace ContactManager.Controllers
-{
-    public class ContactController : ApiController {
-        public string[] Get()
+namespace ContactManager.Controllers {
+    public class ContactController : ApiController
+    {
+        public Contact[] Get()
         {
-            return new string[]
-            {
-                "Hello",
-                "World"
+            return new Contact[] {
+                new Contact {
+                    Id = 1,
+                    Name = "Glenn Block"
+                },
+                new Contact {
+                    Id = 2,
+                    Name = "Dan Roth"
+                }
             };
         }
     }
-
-
 }
